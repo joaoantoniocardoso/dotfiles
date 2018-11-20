1bb8687bdc9545878c2c967ebf728fd536ede1d5
João Antônio Cardoso <joao.maker@gmail.com>
2018-11-20 11:21:12 -0200

    Basic script to start to move dotfiles to git

---
+++ dottogit.sh
@@ -0,0 +1,33 @@
+#!/bin/env sh
+# This script helps to automate my dotfiles:
+# 1) moves the original file to git
+# 2) creates a symbolic link in original place to the original (now on git)
+# 3) uploads to git
+
+# Folder of the git repo
+REPO_DIR=~/dotfiles
+
+if [ $# -lt 1 ]; then
+    echo "One argument needed."
+    exit
+fi
+
+TARGET=$(readlink -f $1)
+target_filename=$(basename $TARGET)
+target_dir=$(dirname $TARGET)
+
+echo Moving $TARGET to $REPO_DIR
+mv $TARGET $REPO_DIR
+echo ... DONE
+echo Linking $REPO_DIR/$target_filename to $target_dir
+ln -sv $REPO_DIR/$target_filename $target_dir
+echo ... DONE
+
+currentdir=$(pwd)
+echo Updating git
+cd $REPO_DIR
+git add $target_filename
+git commit -m "dotfile added"
+git push origin master
+cd $currentdir
+
